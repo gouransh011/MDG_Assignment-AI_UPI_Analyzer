1ee7b9d1a975b04714321b142c8326c4f01e7337
Gouransh <gouranshgupta01@gmail.com>
2026-06-06 15:15:46 +0530

    chore: initialize project skeleton

---
+++ ai_upi_analyzer/android/app/src/main/kotlin/com/example/ai_upi_analyzer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_upi_analyzer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ upi_transaction_parser/example/upi_transaction_parser_example.dart
@@ -0,0 +1,6 @@
+import 'package:upi_transaction_parser/upi_transaction_parser.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}

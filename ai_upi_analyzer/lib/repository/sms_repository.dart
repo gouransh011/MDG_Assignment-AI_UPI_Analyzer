bdc95032595e170b59738a8275f9f4254326af69
Gouransh <gouranshgupta01@gmail.com>
2026-06-08 12:12:16 +0530

    feat: integration upi transaction parser package

--- ai_upi_analyzer/lib/repository/sms_repository.dart
+++ ai_upi_analyzer/lib/repository/sms_repository.dart
@@ -1,28 +1,30 @@
 import 'package:upi_transaction_parser/upi_transaction_parser.dart';
 
 class SmsRepository {
-    Future<List<UpiTransaction>> getTransactions() async {
+  Future<List<UpiTransaction>> getTransactions() async {
+    //since in actual fetching of the data we would have some delay so we are creating that delay by our own for testing
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
 
-         return [
-          UpiTransaction(
-            amount: 500,
-            merchant: "Amazon",
-            type: TransactionType.expense,
-            date: DateTime.now(),
-            category: "Shopping",
-            rawMessage: "Dummy SMS",
-          ),
+    final messages = [
+      'Rs.500 paid to Amazon via UPI',
+      'Rs.1200 received from Salary Account',
+      'INR 250 paid to Swiggy',
+      'Rs.800 credited from Employer',
+    ];
 
-          UpiTransaction(
-            amount: 1200,
-            merchant: "Salary",
-            type: TransactionType.income,
-            date: DateTime.now(),
-            category: "Income",
-            rawMessage: "Dummy SMS",
-          ),
-         ];
+    final List<UpiTransaction> transactions = [];
+
+    for (final message in messages) {
+
+      final transaction = UpiParser.parse(message,DateTime.now());
+
+      if (transaction != null) {
+        transactions.add(transaction);
+      }
     }
 
-    
+    return transactions;
+  }
 }
